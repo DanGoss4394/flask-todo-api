8b8fef7893b96bde8a8df9f4b24542b1f3059da0
Daniel Gosselin <dangoscode@gmail.com>
2021-03-01 11:49:22 -0700

    Add pyscopg2, update app to use environment variable database url

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
-    os.path.join(basedir, "app.sqlite")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
